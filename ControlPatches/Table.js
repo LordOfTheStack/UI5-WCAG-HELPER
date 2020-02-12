908b2222e51f81ab169ec7daf44ab60de097abbb
Lord Phillip Smith <weescript@gmail.com>
2020-02-12 10:36:30 +1000

    Update Table.js
    More testing needed for table column hiding, the table makes unexpected changes to the dom during mode changes

--- ControlPatches/Table.js
+++ ControlPatches/Table.js
@@ -16,12 +16,13 @@ community.wcagHelper.ControlPatches.Table = {
 		this.$().find("td span,th span").removeClass("sapMTextBreakWord"); //wrap text by word, not by letter
 		
 		//properly remove hidden columns for all users
+		/* Requires more testing, to account for all modes and toggling
 		var hiddenCols = this.$().find("th[aria-hidden=true]");
 		var oldColspan = this.$().find("th").length;
 		var newColspan = oldColspan - hiddenCols.length;
 	    this.$().find("table").find("*[aria-hidden=true]:not(.sapUiIcon)").css("display","none");
 	    this.$().find("td[colspan=" + oldColspan + "]").attr("colspan",newColspan); //re-apply any colspans to reflect the changes
-	    
+	    	*/
 	    
 	}
 	
